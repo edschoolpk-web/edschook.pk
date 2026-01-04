edcdfbf84ec2010378b0f5308f95830db5499aaf
Arish Ali <109577410+arishhh@users.noreply.github.com>
2026-01-05 02:30:59 +0500

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  // Force build cache clear: 2026-01-05
   /* config options here */
   experimental: {
     serverActions: {
